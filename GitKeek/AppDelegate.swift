2dbfd7546485a3e7f28c1789570cd3f3a917716d
Sergey <ios.sibedge@gmail.com>
2017-03-06 16:49:54 +0700

    Launch

--- GitKeek/AppDelegate.swift
+++ GitKeek/AppDelegate.swift
@@ -12,10 +12,12 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var startup = StartupService()
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        self.startup.setup()
+        
         return true
     }
 
@@ -27,6 +29,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+    }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+        return true
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
@@ -40,7 +46,5 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
-
-
 }
 
---
+++ GitKeek/StartupService.swift
@@ -0,0 +1,16 @@
+//
+//  StartupService.swift
+//  GitKeek
+//
+//  Created by Sergey on 06.03.17.
+//  Copyright © 2017 Sergey Kim. All rights reserved.
+//
+
+import Foundation
+
+struct StartupService {
+    
+    func setup() {
+        
+    }
+}
